8903383e81d76e7182a6c44c2626637ecd2fbc04
Samet <111511331+amet7@users.noreply.github.com>
2023-02-02 10:03:16 +0300

    Update conf.py

--- docs/en/source/conf.py
+++ docs/en/source/conf.py
@@ -83,7 +83,7 @@ epub_show_urls = 'footnote'
 # a list of builtin themes.
 #
 html_theme = 'default'
-html_logo = '_static/logo_picobricks2.png'
+html_logo = '_static/REX.png'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
